c15b70dae9ec155d0f0a3d28721ec915b8b7345b
h_latifi8 <h.latifi03@gmail.com>
2021-08-12 08:47:57 +0430

    useContext- manage stateHandler in state copm

--- src/Components/Context/CounterOne.js
+++ src/Components/Context/CounterOne.js
@@ -3,12 +3,21 @@ import { useCount, useCountActions } from "./CounterProvider";
 
 const CounterOne = () => {
     const count = useCount();
-    const setCount = useCountActions();
+    const { addOne, addFive } = useCountActions();
+
+    // const addOne = () => {
+    //     setCount(prevCount => prevCount + 1)
+    // }
+    // const addFive = () => {
+    //     setCount(prevCount => prevCount + 5)
+    // }
 
     return (
         <div>
             count is : {count}
-            <button onClick={() => { setCount(count + 1) }} >add one</button>
+            <button onClick={addOne} >add one</button>
+            <button onClick={addFive} >add five</button>
+
 
         </div>
 
